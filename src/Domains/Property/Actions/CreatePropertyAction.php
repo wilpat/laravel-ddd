01101ffd7fec081a5bd4f174c1ea5ca8dba83b67
William <wilpat456@gmail.com>
2023-04-02 13:08:01 +0100

    second commit for branch b

--- src/Domains/Property/Actions/CreatePropertyAction.php
+++ src/Domains/Property/Actions/CreatePropertyAction.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Domains\Property\Actions;
 
 use App\Models\Property;
+use Illuminate\Support\Facades\Log;
 use Infrastructure\Property\Actions\CreatePropertyContract;
 
 class CreatePropertyAction implements CreatePropertyContract
 {
     public function handle(array $propertyAttributes): Property
     {
+        Log::info("Second update for branch b");
         return Property::create($propertyAttributes);
     }
 }
